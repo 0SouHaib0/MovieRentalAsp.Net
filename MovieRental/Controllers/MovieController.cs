96eab09eba62f9ff37afcfaf926b863a88d28d89
0SouHaib0 <elbahrisouhaib@gmail.com>
2022-09-15 00:13:49 +0200

    Set the project environnement

---
+++ MovieRental/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "shrek!" };
+            return View(movie);
+        }
+    }
+}
